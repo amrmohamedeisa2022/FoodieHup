dbaaf92818ecf1b4fa64a93a8431f3a3f4987e62
Elesawy15 <melesawy8122@gmail.com>
2026-02-10 02:26:51 +0200

    Otp

--- src/Routers.jsx
+++ src/Routers.jsx
@@ -1,30 +1,23 @@
-import React from "react";
 import { Route, Routes, Navigate } from "react-router-dom";
 import AdminRoute from "./AdminRoute";
 import CustomerRoute from "./CustomerRoute";
-import { useAuth } from "./context/AuthContext"; 
+import { useAuth } from "./context/AuthContext";
 
 export default function Routers() {
   const { user } = useAuth();
 
   return (
     <Routes>
-      <Route
-        path="/"
-        element={
-          user?.role === "ROLE_RESTAURANT_OWNER" ? (
-            <Navigate to="/admin" replace />
-          ) : (
-            <CustomerRoute />
-          )
-        }
-      />
-
-      
+      {/* Admin */}
       <Route path="/admin/*" element={<AdminRoute />} />
 
-      
+      {/* Customer (كل الموقع) */}
       <Route path="/*" element={<CustomerRoute />} />
+
+      {/* Redirect owner لو دخل / */}
+      {user?.role === "ROLE_RESTAURANT_OWNER" && (
+        <Route path="/" element={<Navigate to="/admin" replace />} />
+      )}
     </Routes>
   );
 }
